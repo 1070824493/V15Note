90a95c012c46dcb53c5b664f8a57442ec23265d6
ty <tangyi.get@gmail.com>
2021-10-24 23:05:38 +0800

    # listview

---
+++ flutter001_listview/android/app/src/main/kotlin/com/example/flutter001_listview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter001_listview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter001_listview/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter001_listview/list_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyListPage()));
+}
